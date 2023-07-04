230c7c9f39a064924248bc7d58d03a03b8cbfde3
Anurag-Dhurwey <anuragdhurwey9211@gmail.com>
2023-07-04 12:34:03 +0530

    little bit home page improved

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -17,7 +17,7 @@ const Home = () => {
   const dispatch = useAppDispatch();
 
   const usersList = async () => {
-    const media = await client.fetch(`*[_type == "post"]`);
+    const media = await client.fetch(`*[_type == "post"]{_id,caption,desc,meadiaFile,postedBy->,tag}`);
     // console.log(media)
     dispatch(set_media_items(media));
     return media;
@@ -30,16 +30,19 @@ const Home = () => {
   return (
     <div className="w-full flex justify-center items-center">
       <div className="w-[15%] h-screen bg-slate-600"></div>
-      <div className="w-[60%] h-screen bg-green-600">
+      <div className="w-[80%] h-screen ">
         {all_Media_content.map((item, i) => {
           const { caption, desc, meadiaFile, postedBy } = item;
+          const {userName}=postedBy
           const url = getFileAsset(meadiaFile.asset, { projectId: "8u4ze61m", dataset: "production",}).url;
           const meadiaType=`${url.substring(url.lastIndexOf(".") + 1)}`
+          console.log(item)
           return (
-            <div key={i}>
+            <div key={i} className="w-full py-2 flex justify-evenly items-start bg-green-600">
+              <div className="w-[65%] h-fit">
               {
                 ['webm','mp4','avi','ogg'].includes(meadiaType) && (
-                  <video controls src={url}/>
+                  <video controls src={url}  />
                 )
               }
 
@@ -48,11 +51,20 @@ const Home = () => {
                   <Image src={url} alt="post" width={1000} height={1000} />
                 )
               }
+              </div>
+              <div className="w-[30%] h-full bg-yellow-700 p-3">
+                <div className="flex justify-start items-center gap-x-2">
+                  <button className="h-8 w-8 bg-blue-600 rounded-3xl"></button>
+                  <h4>{userName}</h4>
+                </div>
+                <div></div>
+                <div></div>
+              </div>
             </div>
           );
         })}
       </div>
-      <div className="w-[15%] h-screen bg-yellow-700"></div>
+  
     </div>
   );
 };
