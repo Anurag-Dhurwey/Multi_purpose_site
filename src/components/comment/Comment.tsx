7f3d464abb47a8a1738f02a657d566456d5c92b5
Anurag-Dhurwey <anuragdhurwey9211@gmail.com>
2023-07-17 12:39:01 +0530

    desired css added to comment box

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -8,64 +8,90 @@ const Comment = ({ comments, user, caption, desc }) => {
 
   return (
     <div className={`w-[30%] h-[395px] overflow-hidden`}>
-     {!cmtView && ( <div
-        className={`p-3 mb-1 ${descView?'h-full':''} bg-yellow-700 rounded-md overflow-auto`}
-        onClick={() => {
-          setDescView(!descView);
-          setCmtView(false);
-        }}
-      >
-        <span className="flex justify-start items-center gap-x-2">
-          <button className="h-8 w-8 bg-blue-600 rounded-3xl"></button>
-          <h4>{user}</h4>
-          <button className="text-2xl font-semibold text-white">
-            {descView ? <MdExpandLess /> : <MdExpandMore />}
-          </button>
-        </span>
-        <h3>{descView ? caption : caption.slice(0, 30) + "....."}</h3>
-        <p className={`text-sm ${descView ? "" : "hidden"}`}>{desc}</p>
-        {descView && (
-          <button className="text-2xl font-semibold text-white">
-            {descView ? <MdExpandLess /> : <MdExpandMore />}
-          </button>
-        )}
-      </div>)}
-
-     {!descView && ( <div className={`w-full ${cmtView?'h-full':''} rounded-md bg-yellow-700 flex flex-col justify-start overflow-auto`}>
+      {!cmtView && (
         <div
-          className={`w-full p-3 pt-0  `}
-          onClick={() => {
-            setCmtView(!cmtView);
-            setDescView(false);
-          }}
+          className={`p-3 pb-0 mb-1 ${
+            descView ? "h-full" : ""
+          } bg-yellow-700 rounded-md overflow-auto`}
         >
-          {comments?.map((cmnt, i) => {
-            const { comment, postedBy: commentBy } = cmnt;
-            const { name: user } = commentBy;
-            return (
-              <div
-                key={comment + i}
-                className={`flex flex-col justify-start items-start ${
-                  cmtView ? " " : i == 0 ? "" : "hidden"
-                }`}
-              >
-                <span className="flex justify-center items-center">
-                  <button className="w-4 h-4 bg-blue-600 rounded-full"></button>
-                  <p>{user}</p>
-                </span>
-                <p>{cmtView ? comment : comment.slice(0, 50) + "....."}</p>
-               
-              </div>
-            );
-          })}
-          {cmtView && (
-                  <div className="w-full ">
-                    <CommentForm />
-                  </div>
-                )}
+          <span className="flex justify-start items-center gap-x-2">
+            <button className="h-8 w-8 bg-blue-600 rounded-3xl"></button>
+            <h4>{user}</h4>
+            {/* <button className="text-2xl font-semibold text-white">
+              {descView ? <MdExpandLess /> : <MdExpandMore />}
+            </button> */}
+          </span>
+          <h3>{descView ? caption : caption.slice(0, 30) + "....."}</h3>
+          <p className={`text-sm ${descView ? "" : "hidden"}`}>{desc}</p>
+          {/* {descView && ( */}
+          <span className="w-full flex justify-center sticky bottom-0">
+            <button
+              className="text-2xl font-semibold text-white"
+              onClick={() => {
+                setDescView(!descView);
+                setCmtView(false);
+              }}
+            >
+              {descView ? (
+                <MdExpandLess className="p-0" />
+              ) : (
+                <MdExpandMore className="p-0" />
+              )}
+            </button>
+          </span>
+          {/* )} */}
         </div>
-        
-      </div>)}
+      )}
+
+      {!descView && (
+        <div
+          className={`w-full ${
+            cmtView ? "h-full" : ""
+          } rounded-md bg-yellow-700 flex flex-col justify-start overflow-auto`}
+        >
+          <div className={`w-full px-3 py-0  `}>
+            {comments?.map((cmnt, i) => {
+              const { comment, postedBy: commentBy } = cmnt;
+              const { name: user } = commentBy;
+              return (
+                <div
+                  key={comment + i}
+                  className={`flex flex-col justify-start items-start ${
+                    cmtView ? " " : i == 0 ? "" : "hidden"
+                  }`}
+                >
+                  <span className="flex justify-center items-center">
+                    <button className="w-4 h-4 bg-blue-600 rounded-full"></button>
+                    <p>{user}</p>
+                  </span>
+                  <p>{cmtView ? comment : comment.slice(0, 50) + "....."}</p>
+                </div>
+              );
+            })}
+          </div>
+         
+          <span className={`w-full ${cmtView?'pt-[80%]':''} flex flex-col items-center justify-center sticky bottom-0`}>
+          {cmtView && (
+            <div className="w-full ">
+              <CommentForm />
+            </div>
+          )}
+            <button
+              className="text-2xl font-semibold text-white"
+              onClick={() => {
+                setCmtView(!cmtView);
+                setDescView(false);
+              }}
+            >
+              {descView ? (
+                <MdExpandLess className="p-0" />
+              ) : (
+                <MdExpandMore className="p-0" />
+              )}
+            </button>
+          </span>
+        </div>
+      )}
       <div></div>
     </div>
   );
--- src/components/comment/miniComps/CommentForm.tsx
+++ src/components/comment/miniComps/CommentForm.tsx
@@ -1,17 +1,34 @@
-import React from "react";
-import {IoMdSend} from 'react-icons/io'
+"use client";
+import React, { useState } from "react";
+import { IoMdSend } from "react-icons/io";
 const CommentForm = () => {
+  const [form, setForm] = useState<String>("");
+
+  const onHnandleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm(e.target.value);
+  };
+
   return (
     <>
-      <form onSubmit={(e)=>console.log(e)} className="mb-1 gap-x-1 w-full flex justify-center items-center">
+      <form
+        onSubmit={(e) => onHnandleSubmit(e)}
+        className="mb-1 gap-x-1 w-full flex justify-center items-center"
+      >
         <input
           type="text"
           name="comment"
           id="comment"
           className="text-xs px-4 py-1 rounded-lg w-[90%] bg-white"
+          value={form}
+          onChange={(e) => onChangeHandler(e)}
         />
         <button type="submit" className="text-xs">
-         <IoMdSend className="text-lg "/>
+          <IoMdSend className="text-lg " />
         </button>
       </form>
     </>
