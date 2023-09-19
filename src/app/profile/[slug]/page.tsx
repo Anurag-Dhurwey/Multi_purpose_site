009d0d8fe7e47a2040b2d167a06ddd77190b8431
Anurag-Dhurwey <anuragdhurwey9211@gmail.com>
2023-09-19 12:09:57 +0530

    resolving error

--- src/app/profile/[slug]/page.tsx
+++ src/app/profile/[slug]/page.tsx
@@ -1,40 +1,46 @@
 "use client";
-import AdminProfile from '@/components/profiles/AdminProfile';
-import UsersProfile from '@/components/profiles/UsersProfile';
-import { useAppDispatch, useAppSelector } from '@/redux_toolkit/hooks';
-import { min_id_of_usr, profileSlugObjType } from '@/typeScript/basics';
-import { socketIoConnection } from '@/utilities/socketIo';
-import { message } from 'antd';
-import { useSession } from 'next-auth/react';
-import React, { useEffect } from 'react'
+import AdminProfile from "@/components/profiles/AdminProfile";
+import UsersProfile from "@/components/profiles/UsersProfile";
+import { useAppDispatch, useAppSelector } from "@/redux_toolkit/hooks";
+import { min_id_of_usr, profileSlugObjType } from "@/typeScript/basics";
+import { socketIoConnection } from "@/utilities/socketIo";
+import { message } from "antd";
+import { useSession } from "next-auth/react";
+import React, { useEffect } from "react";
 
 const Page = ({ params }: { params: { slug: string } }) => {
-    const { data: session } = useSession();
-    const admin = useAppSelector((state) => state.hooks.admin);
-    const dispatch = useAppDispatch();
-    const slug=params.slug
-    const data:profileSlugObjType=JSON.parse(decodeURIComponent(slug));
-    if(!session && !data.email ) return null
-    
-    function withUseEffect() {
-        if (session) {
-          socketIoConnection({
-            session,
-            dispatch,
-            admin,
-            message:message
-          });
-        }
-      }
-    
-      useEffect(() => {
-        withUseEffect()
-       }, [session]);
+  const { data: session } = useSession();
+  const admin = useAppSelector((state) => state.hooks.admin);
+  const dispatch = useAppDispatch();
+  const slug = params.slug;
+  const data: profileSlugObjType = JSON.parse(decodeURIComponent(slug));
+
+  function withUseEffect() {
+    if (session) {
+      socketIoConnection({
+        session,
+        dispatch,
+        admin,
+        message: message,
+      });
+    }
+  }
+
+  useEffect(() => {
+    withUseEffect();
+  }, [session]);
+
+  if (!session && !data.email) return null;
+  
   return (
     <section>
-      {data.email==session?.user?.email?(<AdminProfile/>):(<UsersProfile user={data}/>)}
+      {data.email == session?.user?.email ? (
+        <AdminProfile />
+      ) : (
+        <UsersProfile user={data} />
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
